d4b463c7a33062b8f92405d09172f50d06ce538b
pillarisetty venkata satvika <pvsatvika@gmail.com>
2026-09-18 14:25:18 +0530

    Polish report viewer header visual hierarchy and spacing

--- android/app/src/main/java/com/bugtrace/app/network/ApiConfig.kt
+++ android/app/src/main/java/com/bugtrace/app/network/ApiConfig.kt
@@ -2,5 +2,5 @@ package com.bugtrace.app.network
 
 object ApiConfig {
     // Laptop local IPv4 address on local Wi-Fi network
-    const val BASE_URL = "http://192.168.1.6:8000"
+    const val BASE_URL = "http://192.168.14.94:8000"
 }
