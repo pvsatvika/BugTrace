8f8511d429de16561ad07b7c47dfd544ee40cdbc
pillarisetty venkata satvika <pvsatvika@gmail.com>
2026-09-20 10:10:22 +0530

    Update BASE_URL in ApiConfig.kt to active laptop Wi-Fi IP address (192.168.1.13:8000)

--- android/app/src/main/java/com/bugtrace/app/network/ApiConfig.kt
+++ android/app/src/main/java/com/bugtrace/app/network/ApiConfig.kt
@@ -2,6 +2,6 @@ package com.bugtrace.app.network
 
 object ApiConfig {
     // Laptop local IPv4 address on local Wi-Fi network
-    const val BASE_URL = "http://192.168.1.6:8000"
+    const val BASE_URL = "http://192.168.1.13:8000"
 }
 
